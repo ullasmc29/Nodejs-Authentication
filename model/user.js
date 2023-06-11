3249e5b3e06b5c5f46a98de1ec31c6efc9cd66ba
ullasmc29 <83940712+ullasmc29@users.noreply.github.com>
2023-06-11 12:20:10 +0530

    user.js

---
+++ model/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024, //hash
+        min: 6,
+    }
+    ,
+    date: {
+        type: Date,
+        defauly: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', UserSchema)
